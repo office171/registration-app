33cf1824bd8d52aa679d53d39e4653c12d382207
office171 <office@kvutze.org>
2026-07-09 10:14:02 -0400

    Delete new-chat directory

--- new-chat/registration-app/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT_ID.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
-  STORAGE_BUCKET: "registration-documents",
-  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzQq4llWMXWjr6KPRXgN87-JMBh0wjclY7oJZg44IJ8aFWmmjQFn8_3RsyXPTN7GM5Dtg/exec"
-};
